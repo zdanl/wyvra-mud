89e1bd73427eb89b7114f6a4ff82f2e2da716958
Dan Zulla <dan@mescalin.co>
2022-10-31 05:14:52 +0100

    working login

--- wyvra.py
+++ wyvra.py
@@ -5,22 +5,49 @@
 #   \__/\  /   / ____|  \_/   |__|   (____  /
 #        \/    \/                         \/
 
+
 from wyvra.database import database
 from wyvra.networking import networking
 from wyvra.logging import wyvra_log
 
 import argparse
+
 from _thread import *
 import threading
 
 import yaml, os
 
 print_lock = threading.Lock()
+db = None
 
 def wyvra_handle(c):
     wyvra_log("info", "Handling new connection")
+    c.send(bytes(motd, "utf-8"))
+
+    while True:
+        c.send(b"Nickname: ")
+        nickname = c.recv(128)[:-1]
+        c.send(b"Password: ")
+        password = c.recv(128)[:-1]
+        wyvra_log("info", "Received username and password (%s)" %(nickname))
+    
+        ret = db.check_user(nickname, password)
+        if ret == 2:
+            c.send(b"User identified. You are logged in.\n")
+            c.send(b"*"*80 + b"\n")
+            break
+        elif ret == 1:
+            c.send(b"Incorrect password.\n")
+        else:
+            db.register_user(nickname, password)
+            c.send(b"User registered. You are logged in.\n")
+            wyvra_log("info", "Registered user %s\n" %nickname)
+            c.send(b"*"*80 + b"\n")
+            break
+
     wyvra_log("info", "Receiving data")
     while True:
+        c.send(b"[%s] >> " %nickname)
         data = c.recv(4096)
         if not data:
             print_lock.release()
@@ -32,9 +59,7 @@ def wyvra_handle(c):
     c.close()
 
 def wyvra_init():
-    with open("arts/motd", "r") as motd:
-        motd = motd.read()
-        print(motd)
+    global db
 
     host, port = wyvra_config["host"], wyvra_config["port"]
     if args.port: port = int(args.port)
@@ -43,7 +68,7 @@ def wyvra_init():
     database_name = wyvra_config["database"]
     
     wyvra_log("info", "Connecting database %s" %database_name)
-    db = database(database_name).connection
+    db = database(database_name)
 
     wyvra_log("info", "Starting networking %s:%d" %(host, port))
     net = networking(host, port)
@@ -67,6 +92,9 @@ def wyvra_argparse():
 
 if __name__ == "__main__":
     os.system("clear")
+    with open("arts/motd", "r") as motd:
+        motd = motd.read()
+        print(motd)
     with open("wyvra.yaml", "r") as wyvra_file:
         wyvra_config = yaml.safe_load(wyvra_file)
    
--- wyvra/database/database.py
+++ wyvra/database/database.py
@@ -1,14 +1,37 @@
 from pymongo import MongoClient
 
+import hashlib
+
 class database:
 
     connection_string = "mongodb://localhost:27017/"
     connection = None
+    db = None
 
-    def check_user(user, password):
-        r = self.connection.users.findOne({"nickname": user, "password": password})
+    def _hash_password(self, password):
+        #password = password.encode()
+        hash_obj = hashlib.sha1(password)
+        hexa_value = hash_obj.hexdigest()
+        return hexa_value
+
+    def check_user(self, user, password):
+        success = 0
+        r = self.db.users.find_one({"nickname": user})
         if r:
-            return True
+            success += 1
+            r = self.db.users.find_one({"password": self._hash_password(password)})
+            if r:
+                success += 1
+        return success
+
+    def register_user(self, user, password):
+        hashed = self._hash_password(password)
+        self.db.users.insert({"nickname": user, "password": hashed})
+        return True
+
+    def delete_user(self, user):
+        self.db.users.delete({"nickname": user})
 
     def __init__(self, database_name="wyvra"):
         self.connection = MongoClient(self.connection_string + database_name)
+        self.db = self.connection.wyvra
